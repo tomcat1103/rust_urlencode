513ceff881d2b886363ac7c5fb4ccebadac3a746
tomcat1103 <2731305+tomcat1103@users.noreply.github.com>
2021-11-02 03:52:30 +0900

    add urlencode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
+use std::io;
+
+/// https://url.spec.whatwg.org/#fragment-percent-encode-set
+const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = String::new();
+    io::stdin().read_line(&mut x).expect("Read Error");
+    let out = utf8_percent_encode(&mut x.trim_end(), FRAGMENT);
+    println!("{}", out);
 }
